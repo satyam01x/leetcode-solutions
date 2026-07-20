a652d694d22d676d3d5661299140004a84c886e0
SATYAM <satyam.001x@gmail.com>
2026-07-20 18:52:11 +0530

    Time: 42 ms (90.18%), Space: 57.1 MB (34.81%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+
+    let original = init;
+
+    return{
+
+        increment : function(){
+            init += 1;
+            return init;
+        },
+
+         decrement : function(){
+            init -= 1;
+            return init;
+         },
+
+        reset : function(){
+            init = original;
+            return init;
+         }
+
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
