36b98256009abd815514f92a4ab26faceec77097
SATYAM <satyam.001x@gmail.com>
2026-07-21 18:32:16 +0530

    Time: 40 ms (77.83%), Space: 53.6 MB (58.32%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+
+    return {
+
+        toBe: function(val2) {
+            if (val === val2) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+
+        notToBe: function(val2) {
+            if (val !== val2) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+
+    };
+
+};
