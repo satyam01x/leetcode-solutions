261c0f29cd495c9cf8340021e97dca4ee225b3e3
SATYAM <satyam.001x@gmail.com>
2026-07-27 15:17:44 +0530

    Time: 245 ms (84.18%), Space: 93.8 MB (87.33%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+
+    const cache = {};
+
+    return function (...args) {
+
+        const key = JSON.stringify(args);
+
+        if (key in cache) {
+            return cache[key];
+        }
+
+        const result = fn(...args);
+
+        cache[key] = result;
+
+        return result;
+    };
+}
